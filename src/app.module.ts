90fa09a004b8cf0279e0b725a8f3e7fccca7a4c9
Juca Duarte <jbongiovani@findes.org.br>
2024-11-19 17:24:12 -0300

    Limpeza

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,25 +2,16 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FuelModule } from './application/fuel/fuel.module';
-import { LoggerMiddleware } from './infrastructure/middleware/logger.middleware';
+import { LoggerMiddleware } from './drivers/middleware/logger.middleware';
 import { HydrogenModule } from './application/hydrogen/hydrogen.module';
 import { DieselModule } from './application/diesel/diesel.module';
 import { HybridModule } from './application/hybrid/hybrid.module';
 import { EletricModule } from './application/eletric/eletric.module';
 import { FileModule } from './application/file/file.module';
-import { DatabaseModule } from './infrastructure/database/database.module';
-/*import { ConfigModule } from '@nestjs/config';
-import { envValidate } from './infrastructure/config/env.validations';
-import configuration from './infrastructure/config/configuration';*/
+import { DatabaseModule } from './drivers/database/database.module';
 
 @Module({
   imports: [
-    /*ConfigModule.forRoot({
-      validate: envValidate,
-      envFilePath: '.env',
-      isGlobal: true,
-      load: [configuration],
-    }),*/
     FuelModule,
     HydrogenModule,
     DieselModule,
